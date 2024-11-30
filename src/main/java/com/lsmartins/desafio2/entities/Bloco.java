32ec0e1f485f963dfbb00bafb552212707748a8b
Leandro Martins <leandro2828@gmail.com>
2024-11-30 14:30:58 -0300

    criado entidade categoria e feito relacionamento muitos para um com atividade

--- src/main/java/com/lsmartins/desafio2/entities/Bloco.java
+++ src/main/java/com/lsmartins/desafio2/entities/Bloco.java
@@ -1,6 +1,7 @@
 package com.lsmartins.desafio2.entities;
 
 import java.time.Instant;
+import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -74,6 +75,25 @@ public class Bloco {
 
 	public void setAtividade(Atividade atividade) {
 		this.atividade = atividade;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bloco other = (Bloco) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 	
--- src/main/java/com/lsmartins/desafio2/entities/Categoria.java
+++ src/main/java/com/lsmartins/desafio2/entities/Categoria.java
@@ -10,46 +10,32 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_atividade")
-public class Atividade {
+@Table(name = "tb_categoria")
+public class Categoria {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private String nome;
 	
 	@Column(columnDefinition = "TEXT")
 	private String descricao;
-	private Double preco;
 	
-	@ManyToMany
-	@JoinTable(name = "tb_atividade_participante",
-	joinColumns = @JoinColumn(name = "atividade_id"),
-	inverseJoinColumns = @JoinColumn(name = "participante_id"))
-	private List<Participante> participantes = new ArrayList<>();
+	@OneToMany(mappedBy = "categoria")
+	private List<Atividade> atividades = new ArrayList<>();
 	
-	@OneToMany(mappedBy = "atividade")
-	private List<Bloco> blocos = new ArrayList<>();
-	
-	@ManyToOne
-	@JoinColumn(name = "categoria_id")
-	private Categoria categoria;
-	
-	public Atividade() {
+	public Categoria() {
+		
 	}
 
-	public Atividade(Integer id, String nome, String descricao, Double preco) {
+	public Categoria(Integer id, String descricao) {
+		super();
 		this.id = id;
-		this.nome = nome;
 		this.descricao = descricao;
-		this.preco = preco;
 	}
 
 	public Integer getId() {
@@ -60,14 +46,6 @@ public class Atividade {
 		this.id = id;
 	}
 
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
 	public String getDescricao() {
 		return descricao;
 	}
@@ -76,24 +54,10 @@ public class Atividade {
 		this.descricao = descricao;
 	}
 
-	public Double getPreco() {
-		return preco;
-	}
-
-	public void setPreco(Double preco) {
-		this.preco = preco;
-	}
-
-	public List<Bloco> getBlocos() {
-		return blocos;
-	}
-
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-	
-	
 
 	@Override
 	public boolean equals(Object obj) {
@@ -103,14 +67,9 @@ public class Atividade {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Atividade other = (Atividade) obj;
+		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
 	}
-
-	public List<Participante> getParticipantes() {
-		return participantes;
-	}
-	
 	
 	
 
